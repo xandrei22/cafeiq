a3d543325f6db0871ed3b59bad6c35572094aa44
unknown <alexandrei1628@gmail.com>
2025-10-21 21:03:56 +0800

    Fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -23,10 +23,10 @@ if (process.env.MYSQL_URL) {
 } else {
     // Use individual environment variables
     connectionConfig = {
-        host: process.env.DB_HOST || '',
-        user: process.env.DB_USER || '',
-        password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_NAME || '',
+        host: process.env.DB_HOST || 'rolley.proxy.rlwy.net',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || 'dzHhiqduUpygZMFcXctzLnmwVfARAwes',
+        database: process.env.DB_NAME || 'railway',
         waitForConnections: true,
         connectionLimit: 20,
         queueLimit: 0,
